82012e6dc1bef44be8d06b418f5503751a995712
ochobkaa <creepersyndicate@yandex.ru>
2024-04-15 13:56:44 +0300

    Написал конвертацию между объектами Measure и столбцами датафрейма с возможностью чтения столбца как объектов Measure и записи объектов Measure в датафрейм

--- conversion.py
+++ conversion.py
@@ -3,18 +3,46 @@ import measures as ms
 from typing import Iterable
 
 class MeasureConverter:
-    def __init__(self, df: pd.DataFrame) -> None:
-        self.__df = df
+    def __init__(self) -> None:
+        pass
 
-    def measures(self, name: str, ms_type: type) -> Iterable[ms.Measure]:
-        vals = self.__df.loc[name].to_list()
-
+    def __err_min_col(name: str) -> str:
         err_min_col = "{name}_error_min".format(name=name)
-        err_mins = self.__df.loc[err_min_col].to_list()
-
+        return err_min_col
+    
+    def __err_max_col(name: str) -> str:
         err_max_col = "{name}_error_max".format(name=name)
-        err_maxs = self.__df.loc[err_max_col].to_list()
+        return err_max_col
+        
+    def read(self, name: str, ms_type: type, df: pd.DataFrame) -> Iterable[ms.Measure]:
+        vals = df.loc[name].to_list()
+
+        err_min_col = self.__err_min_col(name)
+        err_mins = df.loc[err_min_col].to_list()
+
+        err_max_col = self.__err_max_col(name)
+        err_maxs = df.loc[err_max_col].to_list()
 
         for val, err_min, err_max in zip(vals, err_mins, err_maxs):
             measure = ms_type(val, err_min, err_max)
             yield measure
+
+    def write(self, name: str, ms_vals: Iterable[ms.Measure], df: pd.DataFrame) -> pd.DataFrame:
+        new_df = df
+        err_min_col = self.__err_min_col(name)
+        err_max_col = self.__err_max_col(name)
+        
+        new_df[name] = df[name].apply(
+            lambda r: next(map(lambda m: m.val, ms_vals)),
+        )
+
+        new_df[err_min_col] = df[err_min_col].apply(
+            lambda r: next(map(lambda m: m.err_min, ms_vals)),
+        )
+
+        new_df[err_max_col] = df[err_max_col].apply(
+            lambda r: next(map(lambda m: m.err_max, ms_vals)),
+        )
+
+        return new_df
+    
