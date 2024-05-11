b511e89e054295d687221cdfa85781321cd5bcfb
ochobkaa <creepersyndicate@yandex.ru>
2024-05-11 14:06:50 +0300

    Изменение метода write, который записывает в датафрейм объекты Measure. Добавлены новые методы для записи объектов Measure по условию и записи одного объекта Measure во все строки, удовлетворяющие условию

--- conversion.py
+++ conversion.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import measures as ms
-from typing import Iterable
+from typing import Iterable, Callable
 
 class MeasureConverter:
     def __init__(self) -> None:
@@ -46,18 +46,51 @@ class MeasureConverter:
         err_max_col = self.__err_max_col(name)
         
         if write_val:
-            col_vals = map(get_val, ms_vals)
-            col_vals_s = pd.Series(col_vals, name=name)
-            new_df.update(col_vals_s)
+            col_vals = list(map(get_val, ms_vals))
+            col_vals_df = pd.DataFrame({name: col_vals}, index=new_df.index)
+            new_df.update(col_vals_df)
 
         if write_err:
-            col_err_min = map(get_err_min, ms_vals)
-            col_err_min_s = pd.Series(col_err_min, name=err_min_col)
-            new_df.update(col_err_min_s)
+            col_err_min = list(map(get_err_min, ms_vals))
+            col_err_min_df = pd.DataFrame({err_min_col: col_err_min}, index=new_df.index)
+            new_df.update(col_err_min_df)
 
-            col_err_max = map(get_err_max, ms_vals)
-            col_err_max_s = pd.Series(col_err_max, name=err_max_col)
-            new_df.update(col_err_max_s)
+            col_err_max = list(map(get_err_max, ms_vals))
+            col_err_max_df = pd.DataFrame({err_max_col: col_err_max}, index=new_df.index)
+            new_df.update(col_err_max_df)
 
         return new_df
     
+    def write_if(self, name: str, ms_vals: list[ms.Measure], df: pd.DataFrame, cond: str,
+              write_val=True, write_err=True) -> pd.DataFrame:
+        new_df = df.copy()
+
+        ms_vals_w = ms_vals
+        slice_df = df.query(cond)
+        
+        slice_df_s =  len(slice_df)
+        ms_vals_s = len(ms_vals_w)
+        if slice_df_s > ms_vals_s:
+            slice_df = slice_df.head(ms_vals_s)
+
+        elif ms_vals_s > slice_df_s:
+            ms_vals_w = ms_vals_w[:slice_df_s]
+
+        new_slice_df = self.write(name, ms_vals, slice_df, write_val=write_val, write_err=write_err)
+        new_df.update(new_slice_df)
+
+        return new_df
+    
+    def write_val_if(self, name: str, ms_val: ms.Measure, df: pd.DataFrame, cond: str,
+              write_val=True, write_err=True) -> pd.DataFrame:
+        new_df = df.copy()
+
+        slice_df = df.query(cond)
+        slice_df_s = len(slice_df)
+
+        ms_val_d = [ms_val for _ in range(slice_df_s)]
+
+        new_slice_df = self.write(name, ms_val_d, slice_df, write_val=write_val, write_err=write_err)
+        new_df.update(new_slice_df)
+
+        return new_df
